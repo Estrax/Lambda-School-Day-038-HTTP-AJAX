4d8438dd6e6b863965fcd186bb1cbcbc7dc7454c
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-27 18:52:30 -0500

    added new data & added conditional rendering to form

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -5,6 +5,7 @@ const FriendForm = props => {
     const id = props.id;
 	return (
 		<form onSubmit={(e) => {e.preventDefault(); setUser({ name: "", email: "", age: 0 })}}>
+            <h1>{props.update ? `Update user with ID: ${id}` : "Add new user"}</h1>
             <input type="text" name="name" placeholder="Name" onChange={(e) => setUser({ ...user, name: e.target.value })} value={user.name} />
             <input type="email" name="email" placeholder="Email" onChange={(e) => setUser({ ...user, email: e.target.value })} value={user.email} />
             <input type="number" name="age" placeholder="Age" onChange={(e) => setUser({ ...user, age: Number(e.target.value) })} value={user.age} />
